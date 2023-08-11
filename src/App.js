a5e8e0314d4edc7ff434090ce0c8cd4ce724c8ed
MarianavCarvalho <marianavcarvalho97@gmail.com>
2023-08-12 00:32:36 +0100

    added API integration

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import Weather from "./Weather.js";
 import './Weather.css';
 
+
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Lisbon"/>
     <footer>
       This project was coded by Mariana Carvalho and is open-sourced on {""}
         <a href="https://github.com/MarianavCarvalho/weather-react-project" target="_blank" rel="noreferrer"> GitHub</a> {""}and hosted on <a
--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+import "./Weather.css";
 
 export default function Weather(){
+    
+    let [weather, setWeather] = useState({ ready: false });
+   
+    function handleResponse(response){
+setWeather({
+    ready: true,
+    temperature: response.data.main.temp,
+    humidity: response.data.main.humidity,
+    wind: response.data.wind.speed,
+    city: response.data.name,
+    date :"Saturday 11:00",
+    description: response.data.weather[0].description,
+    icon: response.data.weather[0].icon
+});
+    }
+
+    if(weather.ready){
     return (
         <div className="Weather">
 <form>
     <div className="row">
         <div className="col-9">
-            <input type="search" placeholder="Type a City..." className="form-control" autocomplete="off"/>
+            <input type="search" placeholder="Type a City..." className="form-control" autoComplete="off"/>
         </div>
         <div className="col-3">
             <input type="submit" value="Search" className="btn btn-primary w-100"/>
@@ -16,28 +35,35 @@ export default function Weather(){
 
     <div className="row two">
         <div className="col-6 cityName">
-            <h1>Lisbon</h1>
+            <h1>{weather.city}</h1>
             <ul className="leftList">
-                <li class="date">Saturday 11:00</li>
+                <li class="date">{weather.date}</li>
                 </ul>
             <ul className="propertiesList">
-                <li>Wind:km/h</li>
-                <li>Humidity:%</li>
+                <li>Wind:{weather.wind}km/h</li>
+                <li>Humidity:{weather.humidity}%</li>
             </ul>
           
         </div>
         <div className="col-6 cityData">
-            <span>12</span>
+            <span>{Math.round(weather.temperature)}</span>
             <span> ºC </span>             
             <ul className="rightList">
-                <li>Clear Sky</li>
+                <li className="text-capitalize">{weather.description}</li>
             </ul>
-             <img src="" alt="" id="icon"/>
+             <img src={weather.icon} alt={weather.description}/>
         </div>
     </div>
-    
-</div>
-
-
+    </div>
     );
+    
+    } else {
+ let apiKey = "866a208a73eeff02182218e9441647a1";
+    let city = "Lisbon";
+let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(handleResponse);
+return (
+    <p>Loading</p>
+)
+    }
 }
