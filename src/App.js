a1e05f2e2113ca280e741633ba4e7014e27d4154
MarianavCarvalho <marianavcarvalho97@gmail.com>
2023-08-11 18:32:35 +0100

    Updated GitHub url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    
+    <footer>
+        <a href="https://github.com/MarianavCarvalho/weather-react-project" target="_blank" rel="noreferrer">Open source</a> {""}project made by Mariana Carvalho
+    </footer>
     </div>
   );
 }
