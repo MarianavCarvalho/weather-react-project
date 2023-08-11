a8b1ae8584710f14f64651441f36e68291c444fe
MarianavCarvalho <marianavcarvalho97@gmail.com>
2023-08-11 18:43:07 +0100

    Updated footer

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
     <footer>
-        <a href="https://github.com/MarianavCarvalho/weather-react-project" target="_blank" rel="noreferrer">Open source</a> {""}project made by Mariana Carvalho
+      This project was coded by Mariana Carvalho and is open-sourced on {""}
+        <a href="https://github.com/MarianavCarvalho/weather-react-project" target="_blank" rel="noreferrer"> GitHub</a> {""}and hosted on <a
+            href="https://weatherreactprojectshecodes.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+             Netlify
+          </a>
     </footer>
+    </div>
     </div>
   );
 }
