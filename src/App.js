ac9c8aa148fbf8d5cf0d4740234b8956b9fb9a5b
MarianavCarvalho <marianavcarvalho97@gmail.com>
2023-08-17 18:58:57 +0100

    updated Netlify url

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
     <footer>
       This project was coded by Mariana Carvalho and is open-sourced on {""}
         <a href="https://github.com/MarianavCarvalho/weather-react-project" target="_blank" rel="noreferrer"> GitHub</a> {""}and hosted on <a
-            href="https://weatherreactprojectshecodes.netlify.app"
+            href="https://glittery-kangaroo-80db37.netlify.app/"
             target="_blank"
             rel="noopener noreferrer"
           >
